0153f2a719770bd1bfe15a6c5af0fee6f0a21261
MarioIliescu <355142@via.dk>
2026-04-22 20:57:18 +0000

    Temperature and plant layout

--- Entities/plant/Plant.cs
+++ Entities/plant/Plant.cs
@@ -1,6 +1,25 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace Entities.plant;
 
 public class Plant
 {
+    [Key] public int Id {get; set;}
+    public string Name { get; set; } = string.Empty;
     
+    //EFC
+    public required string Username {get; set;}
+
+    [ForeignKey(nameof(Username))] public User Owner { get; set; } = null!;
+    
+    public double OptimalTemperature { get; set; }
+    public double OptimalAirHumidity { get; set; }
+    public double OptimalSoilHumidity { get; set; }
+    public double OptimalLightIntensity { get; set; }
+    public ICollection<Temperature> Temperatures { get; set; } = new List<Temperature>();
+    public ICollection<WaterIntake> WaterIntakes { get; set; } = new List<WaterIntake>();
+    public ICollection<SoilHumidity> SoilHumidities { get; set; } = new List<SoilHumidity>();
+    public ICollection<AirHumidity> AirHumidities { get; set; } = new List<AirHumidity>();
+    public ICollection<LightIntensity> LightIntensities { get; set; } = new List<LightIntensity>();
 }
--- Entities/plant/Temperature.cs
+++ Entities/plant/Temperature.cs
@@ -1,6 +1,20 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace Entities.plant;
 
 public class Temperature
 {
+    [Key]
+    public int Id { get; set; }
+    public double? CurrentTemperature { get; set; }
+    public TemperatureScale TemperatureScale { get; set; } = TemperatureScale.C;
+    
+    //EFC
+    
+    public int PlantId { get; set; }
+    
+    [ForeignKey(nameof(PlantId))]
+    public Plant Plant { get; set; } = null!; //For EFC
     
 }
