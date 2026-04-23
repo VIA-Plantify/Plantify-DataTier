66047e769a8ce1a9a01861862f3339a4dbc97b78
PatrikGolian <patrik.golian888@gmail.com>
2026-04-23 14:23:19 +0200

    changed_

--- RepositoryContracts/IPlantRepository.cs
+++ RepositoryContracts/IPlantRepository.cs
@@ -5,9 +5,8 @@ namespace RepositoryContracts;
 public interface IPlantRepository
 {
     Task<Plant> CreateAsync(string username,Plant plant);
-    Task<IEnumerable<Plant>> GetPlantsByUsernameAsync(string username);
+    Task<Plant> GetPlantAsync(string username, int plantId);
     Task DeleteAsync(string username,int plantId);
     Task UpdateAsync(string username,Plant plant);
-    Task<IEnumerable<Plant>> GetManyAsync(string username);
-    Task<IEnumerable<Plant>> GetManyAsync();
+    IQueryable<Plant> GetMany(string username);
 }
