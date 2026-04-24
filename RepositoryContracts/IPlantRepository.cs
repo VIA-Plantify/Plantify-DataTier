fd65b647c3b7473bf34762a2e8256b59808de64d
MarioIliescu <355142@via.dk>
2026-04-24 11:09:02 +0000

    Final change for mac address

--- RepositoryContracts/IPlantRepository.cs
+++ RepositoryContracts/IPlantRepository.cs
@@ -5,8 +5,8 @@ namespace RepositoryContracts;
 public interface IPlantRepository
 {
     Task<Plant> CreateAsync(string username,Plant plant);
-    Task<Plant> GetPlantAsync(string username, int plantId);
-    Task DeleteAsync(string username,int plantId);
+    Task<Plant> GetPlantAsync(string username, string plantMAC);
+    Task DeleteAsync(string username,string plantMAC);
     Task UpdateAsync(string username,Plant plant);
     IQueryable<Plant> GetMany(string username);
 }
