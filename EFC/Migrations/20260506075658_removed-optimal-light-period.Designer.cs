f0992b1ae757fad0a9132af09026a9f447761bda
MarioIliescu <355142@via.dk>
2026-05-06 07:57:09 +0000

    added migration

--- EFC/Migrations/20260506075658_removed-optimal-light-period.Designer.cs
+++ EFC/Migrations/20260506075658_removed-optimal-light-period.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using EFC.DataAccess;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace EFC.Migrations
 {
     [DbContext(typeof(PlantifyContext))]
-    partial class PlantifyContextModelSnapshot : ModelSnapshot
+    [Migration("20260506075658_removed-optimal-light-period")]
+    partial class removedoptimallightperiod
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
