f5995d1f51e40759020f449b00a1c005dcf66c9a
MarioIliescu <355142@via.dk>
2026-05-08 11:28:45 +0000

    Added sensor repository and grpc service

---
+++ GrpcService/Services/ProtoUtils.cs
@@ -0,0 +1,118 @@
+using Entities.plant;
+using Google.Protobuf.WellKnownTypes;
+
+namespace GrpcService.Services;
+
+public static class ProtoUtils
+{
+    /// <summary>
+    /// Maps a Plant entity to an optimal configuration PlantResponse.
+    /// </summary>
+    /// <param name="entity">The Plant entity to map.</param>
+    /// <returns>A PlantResponse object representing the optimal configuration of the plant.</returns>
+    public static PlantResponse MapToOptimalConfiguration(Plant entity)
+    {
+        return new PlantResponse
+        {
+            Username = entity.Username,
+            OptimalTemperature = entity.OptimalTemperature,
+            OptimalAirHumidity = entity.OptimalAirHumidity,
+            OptimalSoilHumidity = entity.OptimalSoilHumidity,
+            OptimalLightIntensity = entity.OptimalLightIntensity,
+            TemperatureScale = (TemperatureScale)entity.Scale
+        };
+    }
+
+    /// <summary>
+    /// Maps a Plant entity to a PlantResponse object.
+    /// </summary>
+    /// <param name="entity">The Plant entity to map.</param>
+    /// <returns>A PlantResponse object representing the mapped plant data.</returns>
+    public static PlantResponse MapToPlantResponse(Plant entity)
+    {
+        var sensorDatas = entity.SensorDatas ?? [];
+        var wateringDatas = entity.Waterings ?? [];
+
+        var response = MapToOptimalConfiguration(entity);
+
+        response.PlantMAC = entity.MAC;
+        response.Name = entity.Name;
+        response.TemperatureScale = (TemperatureScale)entity.Scale;
+
+        var latestSensor = sensorDatas
+            .OrderByDescending(s => s.Id)
+            .FirstOrDefault();
+
+        if (latestSensor is not null)
+        {
+            response.SensorData = MapToSensorResponse(latestSensor);
+        }
+
+        response.PreviousSensorReadings ??= new PreviousSensorResponses();
+
+        response.PreviousSensorReadings.Readings.AddRange(
+            sensorDatas
+                .OrderByDescending(s => s.Id)
+                .Select(MapToSensorResponse)
+        );
+
+        var latestWatering = wateringDatas
+            .OrderByDescending(w => w.Id)
+            .FirstOrDefault();
+
+        if (latestWatering is not null)
+        {
+            response.Watering = MapToWateringResponse(latestWatering);
+        }
+
+        response.PreviousWateringReadings ??= new PreviousWateringResponses();
+
+        response.PreviousWateringReadings.Readings.AddRange(
+            wateringDatas
+                .OrderByDescending(w => w.Id)
+                .Select(MapToWateringResponse)
+        );
+
+        return response;
+    }
+    public static SensorResponse? MapToSensorResponse(SensorData? sensor)
+    {
+        if (sensor == null)
+        {
+            return null;
+        }
+        return new SensorResponse
+        {
+            Id = sensor.Id,
+            Temperature = sensor.Temperature,
+            AirHumidity = sensor.AirHumidity,
+            SoilHumidity = sensor.SoilHumidity,
+            LightIntensity = sensor.LightIntensity,
+            PlantMAC = sensor.PlantMAC,
+            Timestamp = Timestamp.FromDateTime(
+                DateTime.SpecifyKind(sensor.Timestamp, DateTimeKind.Utc)
+            )
+        };
+    }
+
+    public static WateringResponse? MapToWateringResponse(Watering? watering)
+    {
+        if (watering == null)
+        {
+            return null;
+        }
+        return new WateringResponse
+        {
+            Id = watering.Id,
+            PumpTimeInSeconds = watering.PumpTimeInSeconds,
+            WaterLevel = watering.WaterLevel,
+            PlantMAC = watering.PlantMAC,
+            LastWaterTime = Timestamp.FromDateTime(
+                DateTime.SpecifyKind(watering.LastWaterTime, DateTimeKind.Utc)
+            ),
+            PredictedFutureWaterTime = Timestamp.FromDateTime(
+                DateTime.SpecifyKind(watering.PredictedFutureWaterTime, DateTimeKind.Utc)
+            )
+        };
+    }
+}
