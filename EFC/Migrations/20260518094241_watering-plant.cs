24bdfd021d4b153415826f5879b895d220657bc9
MarioIliescu <355142@via.dk>
2026-05-18 09:44:09 +0000

    Migration for new entities

---
+++ EFC/Migrations/20260518094241_watering-plant.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EFC.Migrations
+{
+    /// <inheritdoc />
+    public partial class wateringplant : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PredictedFutureWaterTime",
+                table: "Waterings");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "AddedDate",
+                table: "Plants",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<bool>(
+                name: "ShouldPredictOptimal",
+                table: "Plants",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AddedDate",
+                table: "Plants");
+
+            migrationBuilder.DropColumn(
+                name: "ShouldPredictOptimal",
+                table: "Plants");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "PredictedFutureWaterTime",
+                table: "Waterings",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+    }
+}
